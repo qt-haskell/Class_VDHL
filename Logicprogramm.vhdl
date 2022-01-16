972f83999855f2cc338ceaa3d7814fb1d4af971f
nezha#8294 <93722449+qt-haskell@users.noreply.github.com>
2022-01-16 01:21:09 +0100

    Logic and truetable

---
+++ Logicprogramm.vhdl
@@ -0,0 +1,26 @@
+-- Author: nezha
+-- Date: 15.1.2021
+
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.all
+-------------------------------------------------
+ENTITY io_1 IS
+  Port (
+      el, e2 : IN STD_LOGIC;
+      eing   : IN STD_LOGIC_VECTOR (1 downto 0)
+      al, a2 : OUT STD_LOGIC;
+  );
+END
+------------------------------------------------
+-- Author: nezha
+-- Truetable: io_1
+ARCHITECTURE behaviour OF io_1 IS
+BEGIN
+  CASE eing IS
+  WHEN "00" => a2 <= '0';
+  WHEN "01" => a2 <= '0';
+  WHEN "10" => a2 <= '0';
+  WHEN "11" => a2 <= '1';
+  WHEN others => null;
+END CASE;
+END behaviour
